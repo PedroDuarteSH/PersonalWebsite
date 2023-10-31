8abcccd56e7a8294f77f6a582a6a375c2e3503e8
Pedro Henriques <pedroduartesh@gmail.com>
2023-10-31 00:37:44 +0000

    Updated icons and title

--- src/Information/Information.js
+++ src/Information/Information.js
@@ -26,7 +26,7 @@ const Information = () => {
         <h1 className="Name">Pedro Henriques</h1>
         <p className="Text">
           {text
-            ? "Draw the path from start to know my networks, or click on the button below to know more about me."
+            ? "Draw the path from start to know my networks, or click on the button below to know more about me. But be careful, if you take too long, your drawing will be saved!"
             : "I am a Computer Science student, doing a master's degree in Intelligent Systems. The evolution in appliances of that type of system in recent years impresses me, so I have a large willingness to help in this evolution with new ideas. Through my University path and Internship, have obtained relevant skills in order to start this journey."}
         </p>
         <button className="button" onClick={changeText}>
